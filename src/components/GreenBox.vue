6a267fc5f54081dc04fcf4509ac8e247162da9e6
Diana <diana.sarkisova1999@gmail.com>
2024-03-19 09:48:44 +0300

    init

---
+++ src/components/GreenBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="green__box">
+    <YellowBox />
+  </div>
+</template>
+
+<script setup>
+import YellowBox from './YellowBox.vue';
+
+
+</script>
+
+<style scoped>
+.green__box {
+  color: black;
+  background-color: green;
+  width: 800px;
+  height: 800px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>  
+
