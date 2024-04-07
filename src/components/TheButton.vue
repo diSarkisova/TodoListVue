bddc812ae2821552c4a96ea3ce1988597ebba231
Diana <diana.sarkisova1999@gmail.com>
2024-04-07 16:19:26 +0300

    update dev

---
+++ src/components/TheButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button class="primary-button">
+
+  </button>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+.primary-button {
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 18px 18px;
+  cursor: pointer;
+  transition: border-color 0.25s;
+  background-color: #1E6F9F;
+}
+
+.primary-button:hover {
+  transition: background-color .25s ease-out;
+  background-color: #1E6F9F;
+}
+
+.primary-button:active {
+  background-color: #5E60CE;
+}
+
+.primary-button svg {
+  width: 25px;
+  height: 25px;
+}
+
+.primary-button:hover path {
+  transition: fill .25s ease-out;
+  fill: black;
+}
+</style>
---
+++ src/components/TheInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <input type="text" class="primary-input" :value="modelValue" @input="onInput">
+
+<!--  <input type="text" class="primary-input" v-model="model"/>-->
+</template>
+
+<script setup>
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  }
+})
+const emit = defineEmits(["update:modelValue"])
+
+function onInput (event) {
+  emit('update:modelValue', event.target.value)
+}
+
+// import { defineModel } from 'vue';
+// const model = defineModel()
+
+</script>
+
+<style scoped>
+.primary-input {
+  border: 1px solid transparent;
+  font-size: 18px;
+  line-height: 18px;
+  font-weight: 400;
+  border-radius: 8px;
+  padding: 8px 16px;
+  background-color: #333333;
+  color: #F2F2F2;
+}
+</style>
